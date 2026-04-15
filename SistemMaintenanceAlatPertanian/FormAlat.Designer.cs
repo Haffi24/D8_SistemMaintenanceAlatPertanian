6d16a76362afe23e0bcce5e8fb0849ad2417652f
Haffi24 <haffisaefulloh@gmail.com>
2026-04-16 01:07:27 +0700

    ui: menambahkan tombol aksi simpan, update, dan hapus pada Form Alat

--- SistemMaintenanceAlatPertanian/FormAlat.Designer.cs
+++ SistemMaintenanceAlatPertanian/FormAlat.Designer.cs
@@ -32,6 +32,9 @@
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -49,9 +52,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(68, 48);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.Size = new System.Drawing.Size(56, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Alat";
+            this.label2.Text = "Nama Alat";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // textBox1
@@ -68,11 +71,42 @@
             this.textBox2.Size = new System.Drawing.Size(239, 20);
             this.textBox2.TabIndex = 3;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(573, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 29);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(573, 95);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 29);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Hapus";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(570, 144);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(77, 31);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Simpan";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // FormAlat
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
@@ -90,5 +124,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
