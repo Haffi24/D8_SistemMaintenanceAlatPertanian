7e27d74fe9a31e300519f7a630d94b978f4a88ef
Haffi24 <haffisaefulloh@gmail.com>
2026-05-12 21:09:15 +0700

    feat: membuat view vwDetailMaintenance untuk relasi antar tabel

--- SistemMaintenanceAlatPertanian/UCP2_Query.sql
+++ SistemMaintenanceAlatPertanian/UCP2_Query.sql
@@ -9,3 +9,12 @@ CREATE VIEW vwTeknisiPublic AS
 SELECT id_teknisi, nama_teknisi 
 FROM Teknisi;
 GO
+
+-- View Gabungan Maintenance
+CREATE VIEW vwDetailMaintenance AS
+SELECT 
+    m.id_maintenance, a.nama_alat, t.nama_teknisi, m.tgl_service, m.jenis_perbaikan, m.keterangan
+FROM Maintenance m
+JOIN Alat a ON m.id_alat = a.id_alat
+JOIN Teknisi t ON m.id_teknisi = t.id_teknisi;
+GO
